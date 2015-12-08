9b24505086126a8be3b52e7ce170621625b7e878
Chris <chrisv.perez@gmail.com>
2015-12-08 15:57:09 -0500

    Trying some stuff with http requests. Looks like TwitterAPI needs to be a controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,19 @@
 package controllers
 
+import javax.inject.Inject
+
+import models.TwitterAPI
 import play.api._
+import play.api.libs.ws.WSClient
 import play.api.mvc._
 
-class Application extends Controller {
+class Application @Inject() (ws: WSClient) extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val api = new TwitterAPI(ws)
+    api.authorize()
+//    Ok(api.authorize())
+//    Ok(views.html.index("Your new application is ready."))
   }
 
 }
--- app/models/TwitterAPI.scala
+++ app/models/TwitterAPI.scala
@@ -8,6 +8,7 @@ import scala.concurrent.Future
 import play.api.libs.json._ // JSON library
 import play.api.libs.json.Reads._ // Custom validation helpers
 import play.api.libs.functional.syntax._ // Combinator syntax
+import  scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * Created by chris_000 on 12/5/2015.
@@ -16,11 +17,11 @@ class TwitterAPI @Inject() (ws: WSClient) {
   val TWITTER_API_KEY = "euoX2XoXH9Q5WY4AzVwitVdZf"
   val TWITTER_API_SECRET = "1bxT0CSIcWdNOg9pUhf7wPtsyCzLK99kd7lXRHEdZyK2HgaSs1"
   val TWITTER_API_CREDENTIALS = TWITTER_API_KEY + ":" + TWITTER_API_SECRET
-  val encodedCredentials = BaseEncoding.base64().encode(TWITTER_API_CREDENTIALS.getBytes(Charsets.UTF_8))
+  val encodedCredentials:String = BaseEncoding.base64().encode(TWITTER_API_CREDENTIALS.getBytes(Charsets.UTF_8))
 
   var endpoint: String = "https://api.twitter.com/1.1/search/tweets.json"
 
-  def getTweets(q: String, result_type: String, lat: Int, long: Int, radius: Int): String = {
+  /*def getTweets(q: String, result_type: String, lat: Int, long: Int, radius: Int): String = {
     val url = endpoint + "?q=" + q + "&result_type=" + result_type + "&geocode=" + lat + "," + long + "," + radius + "mi"
     val futureResult: Future[String] = ws.url(url).get().map {
       response =>
@@ -42,14 +43,13 @@ class TwitterAPI @Inject() (ws: WSClient) {
   def getTweetsByHashTag(hashTag: String): String = {
     getTweets("%23" + hashTag, "recent")
   }
-
-  def authorize(): Unit = {
+*/
+  def authorize(): Future[String] = {
     val url = "https://api.twitter.com/oauth2/token"
     val futureResult: Future[String] = ws.url(url)
-      .withHeaders("Authorization" -> encodedCredentials).post().map {
-      response => response.json
-        (response.json \ "person" \ "name").as[String]
+      .withHeaders("Authorization" -> encodedCredentials).get().map {
+      response => response.json.toString()
     }
-    futureResult[String]
+    futureResult
   }
 }
