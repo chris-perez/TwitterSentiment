d5996f8ff81539d01fa65c9a78a8f82da6f1a2df
Luke Waldner <lwaldne1@ithaca.edu>
2015-12-08 17:00:52 -0500

    JS now makes ajax calls on an interval to backend and code is set up to update polygon layer

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,21 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import play.api.libs.json._
 
 class Application extends Controller {
 
   def index = Action {
     Ok(views.html.map())
+  }
+
+  def getPoly() = Action {
+
+    //Json response example -> https://www.playframework.com/documentation/2.2.x/ScalaJson
+    val json: JsValue = JsObject(Seq(
+    "color" -> JsString("#ff0000")
+    ))
+    Ok(json)
   }
 
 }
