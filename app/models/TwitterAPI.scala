056f2b642a5f0f6d2b059ee882c077ed0f25b143
Chris <chrisv.perez@gmail.com>
2015-12-09 16:51:50 -0500

    working on getting tweets after authorizing

--- app/models/TwitterAPI.scala
+++ app/models/TwitterAPI.scala
@@ -5,7 +5,11 @@ import com.google.common.base.Charsets
 import com.google.common.io.BaseEncoding
 import play.api.libs.ws._
 import scala.concurrent.Future
-import play.api.libs.json._ // JSON library
+import play.api.libs.json._
+
+import scala.concurrent.duration.Duration
+
+// JSON library
 import play.api.libs.json.Reads._ // Custom validation helpers
 import play.api.libs.functional.syntax._ // Combinator syntax
 import  scala.concurrent.ExecutionContext.Implicits.global
@@ -21,8 +25,9 @@ class TwitterAPI @Inject() (ws: WSClient) {
   val encodedCredentials:String = BaseEncoding.base64().encode(TWITTER_API_CREDENTIALS.getBytes(Charsets.UTF_8))
 
   var endpoint: String = "https://api.twitter.com/1.1/search/tweets.json"
+  var bearerToken = ""
 
-  /*def getTweets(q: String, result_type: String, lat: Int, long: Int, radius: Int): String = {
+  def getTweets(q: String, result_type: String, lat: Int, long: Int, radius: Int): String = {
     val url = endpoint + "?q=" + q + "&result_type=" + result_type + "&geocode=" + lat + "," + long + "," + radius + "mi"
     val futureResult: Future[String] = ws.url(url).get().map {
       response =>
@@ -33,7 +38,7 @@ class TwitterAPI @Inject() (ws: WSClient) {
 
   def getTweets(q: String, result_type: String): String = {
     val url = endpoint + "?q=" + q + "&result_type=" + result_type
-
+    bearerToken = authorize().result(Duration.Inf)
     val futureResult: Future[String] = ws.url(url).get().map {
       response => response.json
         (response.json \ "person" \ "name").as[String]
@@ -44,12 +49,13 @@ class TwitterAPI @Inject() (ws: WSClient) {
   def getTweetsByHashTag(hashTag: String): String = {
     getTweets("%23" + hashTag, "recent")
   }
-*/
+
   def authorize(): Future[String] = {
     val url = "https://api.twitter.com/oauth2/token"
     val futureResult: Future[String] = ws.url(url)
       .withHeaders("Authorization" -> ("Basic " + encodedCredentials)).post(Map("grant_type"-> Seq("client_credentials"))).map(
-        response => "Response: " + response + "\nBody: " + response.body.toString
+          response => (response.json \ "access_token").as[String]
+//        response => "Response: " + response + "\nBody: " + response.body.toString
       )
     futureResult
   }
