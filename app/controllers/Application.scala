d8222528aa279127da0e73c5ee0b9427e5d26140
cperez3 <chrisv.perez@gmail.com>
2015-12-10 19:19:27 -0500

    have a somewhat working map!

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -17,6 +17,7 @@ import scala.concurrent.duration._
 
 
 class Application @Inject() (ws: WSClient) extends Controller {
+  val api = new TwitterAPI(ws)
 
   def index = Action {
     Ok(views.html.map())
@@ -25,6 +26,7 @@ class Application @Inject() (ws: WSClient) extends Controller {
   def getPoly() = Action {
 
     //Json response example -> https://www.playframework.com/documentation/2.2.x/ScalaJson
+
     val json: JsValue = JsObject(Seq(
     "Colorado" -> JsString("#ff0000"),
     "Idaho" -> JsString("#7f00ff")
@@ -50,21 +52,29 @@ class Application @Inject() (ws: WSClient) extends Controller {
     hexVal
   }
 
-  def sentimentMap(lst: Map[String, List[Tweet]]) : JsArray = {
+  def sentimentMap(lst: Map[String, List[Tweet]]) : JsObject = {
     var sentiment_by_state = Map[String, Float]()
-    var color_by_state:JsArray = Json.arr()
+    var color_by_state:JsObject = Json.obj()
     for((state , value) <- lst) {
+      println("State: " + state)
         val len : Float = value.length
         for(tweet <- value){
-            val current_avg: Float = sentiment_by_state(tweet.state.name)
-            val delta_avg = NlpProcessor.getSentiment(tweet.text)/len
-            sentiment_by_state += (state -> (current_avg + delta_avg))
-
+          val current_avg = 0
+          if (sentiment_by_state.contains(state)) {
+            val current_avg: Float = sentiment_by_state(state)
+          }
+          val delta_avg = NlpProcessor.getSentiment(tweet.text)/len
+          sentiment_by_state += (state -> (current_avg + delta_avg))
         }
+      if (! sentiment_by_state.contains(state)) {
+        sentiment_by_state += (state -> 2.0f)
+      }
      //val temp: String = state
      val color =sentimentToColor(sentiment_by_state(state))
-     color_by_state.append(Json.obj("state"-> state, "color"-> color))
+      color_by_state += (state -> JsString(color))
+//     color_by_state.append(Json.obj(state -> color))
     }
+
     color_by_state
   }
   def getAvgSentiment( lst: List[Tweet]) :Double = {
@@ -78,7 +88,6 @@ class Application @Inject() (ws: WSClient) extends Controller {
     sum/count
   }
   def getTweets() = Action {
-    val api = new TwitterAPI(ws)
     val tweetMap = api.getStateTweets("donaldtrump")
     var map:Map[String, JsArray] = Map()
 
@@ -95,10 +104,11 @@ class Application @Inject() (ws: WSClient) extends Controller {
   }
 
   def query(query : String) = Action {
-    val json: JsValue = JsObject(Seq(
+    val json = sentimentMap(api.getStateTweets(query))
+    /*val json: JsValue = JsObject(Seq(
       "Colorado" -> JsString("#ff0000"),
       "Idaho" -> JsString("#7f00ff")
-    ))
+    ))*/
     Ok(json)
   }
 
